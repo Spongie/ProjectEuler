22b3a773b5767dc38aec2c7dbce97c98edb31596
Philip Persson <kalllez123@hotmail.se>
2017-11-05 19:19:35 +0100

    Problem 9

---
+++ ProjectEuler/Problems/Problem9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem9 : Problem
+    {
+        protected override long DoSolutionSolve()
+        {
+            for (int a = 1; a < 1000; a++)
+            {
+                for (int b = 2; b < 1000; b++)
+                {
+                    for (int c = 3; c < 1000; c++)
+                    {
+                        if (a + b + c != 1000)
+                            continue;
+
+                        if (a * a + b * b == c * c)
+                        {
+                            Progress = 100;
+                            return a * b * c;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("You suck");
+        }
+    }
+}
