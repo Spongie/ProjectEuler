4327f66f2b49c247638ed54db72b4bc61cbb2e33
Philip Persson <kalllez123@hotmail.se>
2017-11-23 23:34:49 +0100

    Problem 16

---
+++ ProjectEuler/Problems/Problem16.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem16 : Problem
+    {
+        protected override long DoSolutionSolve()
+        {
+            return new BigInteger(Math.Pow(2, 1000)).ToString().ToCharArray().Select(x => Convert.ToInt64(x.ToString())).Sum();
+        }
+    }
+}
