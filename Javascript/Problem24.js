02d2c5880e4796653818adb554d1bbb410ddb883
Philip Persson <philip.persson@thirdbase.se>
2018-01-18 22:54:01 +0100

    Problem 24: Brute force is the best force

---
+++ Javascript/Problem24.js
@@ -0,0 +1,78 @@
+//Remember n!
+
+function calcFac(number) {
+    if (number === 1)
+        return 1;
+    
+    return number * calcFac(number -1 );
+}
+
+function replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
+  }
+
+function solve() {
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let totalPermutationsFound = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        let permutations = calcFac(numbers.length - 1);
+
+        if (permutations + totalPermutationsFound > 1000000) {
+            let startNumber = numbers[i];
+
+            let currentNumber = startNumber.toString();
+            for (let x = 0; x < numbers.length; x++) {
+                if (numbers[x] === startNumber)
+                    continue;
+
+                currentNumber += numbers[x].toString();
+            }
+
+            currentNumber = parseInt(currentNumber) - 1;
+            
+            let currentNumberString = currentNumber.toString();
+            startNumber = startNumber.toString();
+            while (currentNumberString[0] == startNumber) {
+                currentNumber++;
+                let foundNumbers = [];
+                let goodNumber = true;
+                currentNumberString = currentNumber.toString();
+                for(let x = 0; x < currentNumberString.length; x++) {
+                    if (!foundNumbers.includes(currentNumberString[x]))
+                        foundNumbers.push(currentNumberString[x]);
+                    else {
+                        goodNumber = false;
+                        break;
+                    }
+                }
+
+                if (!goodNumber)
+                    continue;
+
+                totalPermutationsFound++;
+                if (totalPermutationsFound % 10000 === 0)
+                    console.log(totalPermutationsFound);
+
+                if (totalPermutationsFound === 1000000) {
+                    console.log(currentNumberString);
+                    break;
+                }
+            }
+        }
+        else {
+            totalPermutationsFound += permutations;
+        }
+    }
+}
+/*
+let s = "1234";
+let index = 3;
+let temp = s[index];
+
+let m = replaceAt(s, index, s[index - 1]);
+m = replaceAt(m, index - 1, temp);
+
+console.log(m);
+*/
+solve();
